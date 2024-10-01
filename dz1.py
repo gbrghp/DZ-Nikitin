f8051288a3c7233998d8d7b1c91838d5088adc95
Nikitin Sergei <nikitin.sergey.228@gmail.com>
2024-10-01 15:20:39 +0300

    new file

---
+++ dz1.py
@@ -0,0 +1,13 @@
+#задача 1
+import math
+R = int(input('R: \n'))
+print(round(2*math.pi*R, 2))
+print(round(math.pi*(R**2), 2))
+
+
+# задача 3
+import math
+g = 9.81
+L = int(input('Введите L:'))
+T = 2*math.pi*math.sqrt(L/g)
+print (round(T, 2))
