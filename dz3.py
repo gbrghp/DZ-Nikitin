1536860167145df2ab4f42a42d528ce4beea7b07
Nikitin Sergei <nikitin.sergey.228@gmail.com>
2024-12-03 21:22:11 +0300

    new file

--- dz3.py
+++ dz3.py
@@ -1,8 +1,15 @@
 #задача 1
-n = int(input("Введите число"))
-sum = 0
-for i in range(1, n + 1):
-    sum += i ** 3
-print(sum)
+a = int(input())
+total = 0
+for i in range(1, a):
+    total = total + i ** 3
+print(total)
 
+#задача 2
+for i in range(1, 10):
+
+    for j in range(1, 10):
+        print(i, 'x', j, '=', i * j, end="  ")
+
+    print()
 
